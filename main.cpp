90881cf68e452a03580239dea7799767775a3b12
aleksandar-stefanovic <theonewithideas@gmail.com>
2019-12-07 16:18:40 +0100

    Simplify the code related to QQmlApplicationEngine usage.

--- main.cpp
+++ main.cpp
@@ -5,22 +5,18 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty()) {
+        return -1;
+    }
 
     MainModel mainModel;
 
-    auto *playButton = engine.rootObjects().first()->findChild<QObject *>("play_button");
+    QObject *rootObject = engine.rootObjects().first();
+    auto *playButton = rootObject->findChild<QObject *>("play_button");
     QObject::connect(playButton, SIGNAL(clicked()), &mainModel, SLOT(playSomething()));
 
     return app.exec();
