0ba0f819b6d4e59def7d947d6396902cd2c0f63a
mandja96 <mandja96@gmail.com>
2019-12-10 19:20:36 +0100

    Fix the glitching sound between notes.

--- playback/playback.cpp
+++ playback/playback.cpp
@@ -9,16 +9,24 @@
 sf::Sound sound;
 std::unique_ptr<sf::SoundBuffer> buffer;
 
+// Empirically chosen
+const double ENDING_INTERVAL = 44100 * 0.005;
+
 static std::vector<sf::Int16> create_sample(unsigned frequency = 440, double duration = 10, unsigned sample_rate = 44100) {
+    static double last_value;
 
     std::vector<sf::Int16> samples(duration * sample_rate);
 
     unsigned samples_per_phase = sample_rate / frequency;
 
     for (unsigned long i = 0; i < samples.size(); ++i) {
-        double rad_value_inside_phase = ((double) i / (double) samples_per_phase) * M_PI * 2;
+        unsigned long i_phased = i % samples_per_phase;
+        double rad_value_inside_phase = last_value + ((double) i_phased / (double) samples_per_phase) * M_PI * 2;
         samples[i] = pow(2, 15) * sin(rad_value_inside_phase);
     }
+
+    // This is equal to the value of the rad_value_inside_phase of the last iteration
+    last_value = last_value + ((double) ((samples.size() - 1) % samples_per_phase) / (double) samples_per_phase) * M_PI * 2;
 
     return samples;
 }
@@ -30,6 +38,11 @@ static sf::SoundBuffer bufferFromFrequencies(const std::vector<float>& freqs, fl
     for (const auto &freq : freqs) {
         auto tmp_sample = create_sample(freq, duration);
         samples.insert(samples.end(), tmp_sample.begin(), tmp_sample.end());
+    }
+
+    // Lower the volume of the ending interval to avoid glitching sounds
+    for (auto i = (unsigned long) (samples.size() - ENDING_INTERVAL); i < samples.size(); i++) {
+        samples[i] = static_cast<float>(samples[i]) * static_cast<float>((samples.size() - i)) / ENDING_INTERVAL;
     }
 
     sf::SoundBuffer my_buffer;
