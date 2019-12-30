2c7fc934dad07653dd9a07718cc83cd17419eabb
beskonacnost <kristinapantelic@gmail.com>
2019-12-30 01:00:21 +0100

    Add frequency calculation function according to selected octave and current frequency.

--- src/ui_model/main_model.hpp
+++ src/ui_model/main_model.hpp
@@ -14,6 +14,8 @@ Q_PROPERTY(QList<Track*> tracks READ tracks WRITE set_tracks NOTIFY onTracksChan
 
 public:
     QTimer* timer = new QTimer(this);
+    int current_octave = 4;
+    int fixed_octave = 4;
 
     bool isRecording() {
         return _isRecording;
