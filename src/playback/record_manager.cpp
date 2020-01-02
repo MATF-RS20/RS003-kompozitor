0d283a97c97eb3d6052e1b81498551cfb66f23d3
mandja96 <mandja96@gmail.com>
2020-01-02 18:21:04 +0100

    Add RecordManager methods.

--- src/playback/record_manager.cpp
+++ src/playback/record_manager.cpp
@@ -6,9 +6,65 @@ RecordManager &RecordManager::get_instance() {
 }
 
 void RecordManager::stop_recording() {
+    _end_time = std::chrono::system_clock::now();
+
+    std::chrono::duration<float> time_diff = relative_time(_start_time, _end_time);
+    float time_diff_secs = time_diff.count();
+    std::cout << "End time registered: " << time_diff_secs << '\n';
+
+//    for (int i = 0; i < _track_final_data.size(); i++){
+//        std::cout << "Note info: " << _track_final_data.at(i).pitch() << std::endl;
+//    }
 
 }
 
 void RecordManager::start_recording() {
+    _start_time = std::chrono::system_clock::now();
 
+    std::chrono::duration<float> time_diff = relative_time(_start_time, _start_time);
+    float time_diff_secs = time_diff.count();
+    std::cout << "Start time registered: " << time_diff_secs << '\n';
+}
+
+void RecordManager::add_note(double freq) {
+    NoteEvent currentNote;
+    currentNote.freq = freq;
+    currentNote.note_time_point = std::chrono::system_clock::now();
+    currentNote.current_state = DOWN;
+
+    _record_data.push_back(currentNote);
+}
+
+void RecordManager::remove_note(double freq) {
+    NoteEvent currentNote;
+    currentNote.freq = freq;
+    currentNote.note_time_point = std::chrono::system_clock::now();
+    currentNote.current_state = UP;
+
+    for (auto beg = _record_data.begin(); beg < _record_data.end(); beg++) {
+        if (beg->freq == currentNote.freq){
+            TrackNote tmp(currentNote.freq,
+                          relative_time(_start_time, beg->note_time_point).count(),
+                          relative_time(_start_time, currentNote.note_time_point).count());
+
+            _record_data.erase(beg);
+            std::cout << "Note info:  " <<  tmp.pitch() << "    "
+                      << tmp.start() << "    " << tmp.end() << std::endl;
+
+            // TODO figure out why push_back is not working
+//            _track_final_data.push_back(TrackNote(2,3,4));
+            break;
+        }
+    }
+}
+
+std::chrono::duration<float> RecordManager::relative_time(std::chrono::time_point<std::chrono::system_clock> start_time,
+                                                          std::chrono::time_point<std::chrono::system_clock> compare_time) {
+    std::chrono::duration<long long int, std::__ratio_gcd<std::micro, std::micro>::type> dur{};
+    dur =  compare_time - start_time;
+
+    std::chrono::duration<float> f_secs{};
+    f_secs = std::chrono::duration_cast<std::chrono::duration<float>>(dur);
+
+    return f_secs;
 }
--- src/playback/record_manager.hpp
+++ src/playback/record_manager.hpp
@@ -1,6 +1,22 @@
 #ifndef KOMPOZITOR_RECORD_MANAGER_HPP
 #define KOMPOZITOR_RECORD_MANAGER_HPP
 
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <vector>
+#include <src/ui_model/track_note.hpp>
+
+enum State {
+    UP,
+    DOWN
+};
+
+struct NoteEvent {
+    double freq = 0.0;
+    std::chrono::time_point<std::chrono::system_clock> note_time_point;
+    State current_state;
+};
 
 class RecordManager {
 public:
@@ -15,11 +31,18 @@ public:
     void start_recording();
     void stop_recording();
 
+    void add_note(double freq);
+    void remove_note(double freq);
+
+    static std::chrono::duration<float> relative_time(std::chrono::time_point<std::chrono::system_clock> start_time,
+                                                      std::chrono::time_point<std::chrono::system_clock> compare_time);
 private:
-    RecordManager() {
+    RecordManager() = default;
 
-    }
+    std::vector<NoteEvent> _record_data;
+    std::vector<TrackNote> _track_final_data;
+    std::chrono::time_point<std::chrono::system_clock> _start_time;
+    std::chrono::time_point<std::chrono::system_clock> _end_time;
 };
-
 
 #endif //KOMPOZITOR_RECORD_MANAGER_HPP
